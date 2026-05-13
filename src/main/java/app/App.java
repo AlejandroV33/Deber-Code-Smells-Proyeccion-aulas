d52d9610e77c08f9fedc32d66831ed7d4d5819e0
nathalygaibor <nathalygaibor2002@gmail.com>
2026-05-12 21:03:59 -0500

    Ajustes en App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,11 +1,12 @@
 package app;
 
+import java.io.IOException;
+
 import app.util.DatabaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class App extends Application {
 
