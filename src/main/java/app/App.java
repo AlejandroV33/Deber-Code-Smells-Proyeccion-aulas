bbf289d0b997c8c61c5dd9dfb96133c3e195af0a
7Lemaitre7 <SantyGriez07@outlook.com>
2026-05-12 23:04:31 -0500

    Refactor selected Sonar smells

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] _) {
         launch();
     }
 }
